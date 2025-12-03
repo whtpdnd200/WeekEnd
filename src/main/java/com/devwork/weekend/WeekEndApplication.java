83b945064825020ed788f144553abd052f09df80
whtpdnd200 <whtpdnd200@gmail.com>
2025-12-03 20:05:05 +0900

    [조세웅] - 1차 프로젝트 깃 연동

---
+++ src/main/java/com/devwork/weekend/WeekEndApplication.java
@@ -0,0 +1,13 @@
+package com.devwork.weekend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeekEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeekEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devwork/weekend/WeekEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devwork.weekend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeekEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
