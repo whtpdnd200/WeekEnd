f8578ccc5311d2567c26cb4198e15364884fa14c
whtpdnd <whtpdnd200@gmail.com>
2025-12-16 16:06:57 +0900

    [조세웅] - 브랜치 임포트 항목 수정

--- src/main/java/com/devwork/weekend/post/repository/PostRepository.java
+++ src/main/java/com/devwork/weekend/post/repository/PostRepository.java
@@ -1,12 +1,8 @@
 package com.devwork.weekend.post.repository;
 
 import com.devwork.weekend.post.domain.Post;
-import com.devwork.weekend.post.postDTO.PostListDTO;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import java.util.List;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
